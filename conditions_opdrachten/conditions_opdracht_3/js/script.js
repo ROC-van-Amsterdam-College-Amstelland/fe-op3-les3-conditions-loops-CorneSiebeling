762bd7422826bfc92726e583b3e4d3d245a56c80
Corné Siebeling <2078538@talnet.nl>
2019-11-25 15:10:43 +0100

    opdracht 3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,12 @@
-//javascript code
+function check() {
+    var saldo = document.getElementById("saldo").value  
+
+
+    if (saldo == 500) {
+    document.getElementById("tekst").innerHTML= "helaas geen geld";
+    }else if (saldo <500){
+    document.getElementById("tekst").innerHTML= "er is gestort met bonus";
+    }else{
+    document.getElementById("tekst").innerHTML= "uw slaris is gestort";
+    }
+    }
