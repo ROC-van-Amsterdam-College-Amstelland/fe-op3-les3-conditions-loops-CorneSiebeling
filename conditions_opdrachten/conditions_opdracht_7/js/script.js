704b5a4bc91f196797d7a6e4b93a7aeb24ccad1a
Corné Siebeling <2078538@talnet.nl>
2019-11-26 13:33:00 +0100

    opdracht 7

--- conditions_opdrachten/conditions_opdracht_7/js/script.js
+++ conditions_opdrachten/conditions_opdracht_7/js/script.js
@@ -1,1 +1,16 @@
-//javascript code
+function check() {
+    var saldo = document.getElementById("saldo").value  
+
+    //hier heb ik neer gezet dat als het getal precies 500 is komt er "uw salaris is gestort" te staan
+        if (saldo == 500) {
+    document.getElementById("tekst").innerHTML= "uw salaris is gestort";
+    }
+    //hier heb ik neer gezet dat als het getal hoger is dan 500 dan "uw salaris is gestort met bonus" te staan
+    else if (saldo >500){
+    document.getElementById("tekst").innerHTML= "uw salaris is gestort met bonus";
+    }
+    // hier heb ik neer gezet dat als het getal anders is dan opgegeven wat hier boven staat. in dit geval minder dan 500. komt er "helaas geen geld" te staan
+    else{
+    document.getElementById("tekst").innerHTML= "helaas geen geld";
+    }
+    }
